09c3b4735020a1d960576343b192f35a681bb3df
IvoTab11 <142843560+IvoTab11@users.noreply.github.com>
2023-11-03 22:50:22 -0300

    Se intentan destruir los barriles al llegar al final de la escena

--- Proyecto2D-IvoTabarcache/Assets/Scripts/Spawner.cs
+++ Proyecto2D-IvoTabarcache/Assets/Scripts/Spawner.cs
@@ -5,19 +5,35 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public GameObject prefab;
+    //private GameObject barril;
     void Start()
     {
         Spawn();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 
     public void Spawn(){
         Instantiate(prefab, transform.position, Quaternion.identity);
         Invoke(nameof(Spawn), Random.Range(2f,4f));
+
+        // if ((transform.position.x >= -6.0f) && (transform.position.y>=-6.0f))
+        // {
+        //     // Destruye el objeto cuando alcanza la posición destino.
+        //     Destroy(prefab);
+        // }
+        // if ((barril.transform.position.x >= -6.0f) && (barril.transform.position.y >= -6.0f))
+        // {
+        //     Destroy(barril);
+        // }
     }
+
+    // void Update()
+    // {
+    //     if (barril != null && (barril.transform.position.x >= -6.0f) && (barril.transform.position.y >= -6.0f))
+    //     {
+    //         Destroy(barril);
+    //     }
+    // }
 }
