458dbfa2a9f58d73c0b27d36fab005e40ccce060
IvoTab11 <142843560+IvoTab11@users.noreply.github.com>
2023-11-06 10:16:21 -0300

    Se agregó la animación de Fire. Se programó para que pueda mirar hacia el lado en el que esta avanzando

--- Proyecto2D-IvoTabarcache/Assets/Scripts/Fire.cs
+++ Proyecto2D-IvoTabarcache/Assets/Scripts/Fire.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Fire : MonoBehaviour
 {
-    public float velocidad = 2.0f;
+     public float velocidad = 2.0f;
     public float distanciaMaxima = 10.0f;
 
     private Vector3 inicioPosicion;
@@ -28,43 +28,50 @@ public class Fire : MonoBehaviour
             // Cambiar la dirección y la posición de destino
             direccion = -1;
             destinoPosicion = inicioPosicion;
+            // Rotar el objeto para que mire en la dirección opuesta
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
         }
         else if (direccion == -1 && transform.position.x <= inicioPosicion.x)
         {
             // Cambiar la dirección y la posición de destino
             direccion = 1;
             destinoPosicion = inicioPosicion + Vector3.right * distanciaMaxima;
+            // Restaurar la rotación para que mire en la dirección original
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
         }
     }
-    //  public float velocidad = 2.0f;
+    
+    // public float velocidad = 2.0f;
     // public float distanciaMaxima = 10.0f;
 
     // private Vector3 inicioPosicion;
     // private Vector3 destinoPosicion;
+    // private int direccion = 1;
 
     // void Start()
     // {
     //     inicioPosicion = transform.position;
     //     destinoPosicion = inicioPosicion + Vector3.right * distanciaMaxima;
-
-    //     StartCoroutine(MoverPersonaje());
     // }
 
-    // IEnumerator MoverPersonaje()
+    // void Update()
     // {
-    //     while (true)
-    //     {
-    //         while (transform.position != destinoPosicion)
-    //         {
-    //             transform.position = Vector3.MoveTowards(transform.position, destinoPosicion, velocidad * Time.deltaTime);
-    //             yield return null;
-    //         }
+    //     // Mover el personaje en la dirección actual
+    //     transform.Translate(Vector3.right * direccion * velocidad * Time.deltaTime);
 
-    //         // Intercambiar las posiciones de inicio y destino para el movimiento de regreso
-    //         Vector3 temp = inicioPosicion;
-    //         inicioPosicion = destinoPosicion;
-    //         destinoPosicion = temp;
-    //         yield return null;
+    //     // Comprobar si se ha alcanzado la posición de destino
+    //     if (direccion == 1 && transform.position.x >= destinoPosicion.x)
+    //     {
+    //         // Cambiar la dirección y la posición de destino
+    //         direccion = -1;
+    //         destinoPosicion = inicioPosicion;
+    //     }
+    //     else if (direccion == -1 && transform.position.x <= inicioPosicion.x)
+    //     {
+    //         // Cambiar la dirección y la posición de destino
+    //         direccion = 1;
+    //         destinoPosicion = inicioPosicion + Vector3.right * distanciaMaxima;
     //     }
     // }
+    
 }
